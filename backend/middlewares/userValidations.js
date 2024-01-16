5a564ee46b95a2f22e06387ad11bba91d3c833db
Rafael Lebre <rafaellebre100@gmail.com>
2024-01-16 19:17:40 +0100

    190: Validações de criação de usuário

---
+++ backend/middlewares/userValidations.js
@@ -0,0 +1,34 @@
+const { body } = require("express-validator")
+
+const userCreateValidation = () => {
+    return [
+        body("name")
+            .isString()
+            .withMessage("The name is mandatory")
+            .isLength({ min: 3 })
+            .withMessage("The name must have at least 3 characters."),
+        body("email")
+            .isString()
+            .withMessage("The e-mail is mandatory.")
+            .isEmail()
+            .withMessage("Insert a valid e-mail address"),
+        body("password")
+            .isString()
+            .withMessage("The password is mandatory.")
+            .isLength({min: 5})
+            .withMessage("The password must have at least 5 characters."),
+        body("confirmpassword")
+            .isString()
+            .withMessage("The password confirmation is mandatory.")
+            .custom((value, {req}) => {
+                if(value != req.body.password) {
+                    throw new Error("The passwords do not match.")
+                }
+                return true
+            })
+        ]
+}
+
+module.exports = {
+    userCreateValidation,
+}
