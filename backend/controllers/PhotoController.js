3dc370fd37ad9cf1355b98b8b9cfbcd9dbb77104
Rafael Lebre <rafaellebre100@gmail.com>
2024-01-20 12:38:48 +0100

    201: Inserindo foto

---
+++ backend/controllers/PhotoController.js
@@ -0,0 +1,38 @@
+const Photo = require("../models/Photo")
+
+const mongoose = require("mongoose")
+const User = require("../models/User")
+
+// Insert a photo, with an user related to it
+const insertPhoto = async(req, res) => {
+
+    const {title} = req.body
+    const image = req.file.filename
+
+    const reqUser = req.user
+
+    const user = await User.findById(reqUser._id)
+ 
+    // Create a photo
+    const newPhoto = await Photo.create({
+        image, 
+        title,
+        userId: user._id,
+        userName: user.name,
+    })
+
+    //If photo was create succesfully, return data
+    if(!newPhoto) {
+
+        res.status(422).json({
+            errors: ["There's been an error, please try again later."]
+        })
+    }
+
+
+    res.status(201).json(newPhoto)
+}
+
+module.exports = {
+    insertPhoto,
+}
