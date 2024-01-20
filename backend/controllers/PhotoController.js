3ef8c936a97e559c3aeafcff8db5ec8c88b00a4a
Rafael Lebre <rafaellebre100@gmail.com>
2024-01-20 21:55:29 +0100

    205: Resgatando foto por ID

--- backend/controllers/PhotoController.js
+++ backend/controllers/PhotoController.js
@@ -1,82 +1,109 @@
 const Photo = require("../models/Photo");
-const User = require("../models/User");
+const User = require("../models/User")
+
+const mongoose = require("mongoose");
 
 // Insert a photo, with an user related to it
 const insertPhoto = async (req, res) => {
-    const { title } = req.body;
-    const image = req.file.filename;
-    const reqUser = req.user;
+  const { title } = req.body;
+  const image = req.file.filename;
 
-    const user = await User.findById(reqUser._id);
+  console.log(req.body);
 
-    // Create a photo
-    const newPhoto = await Photo.create({
-        image, 
-        title,
-        userId: user._id,
-        userName: user.name,
+  const reqUser = req.user;
+
+  const user = await User.findById(reqUser._id);
+
+  console.log(user.name);
+
+  // Create photo
+  const newPhoto = await Photo.create({
+    image,
+    title,
+    userId: user._id,
+    userName: user.name,
+  });
+
+  // If user was photo sucessfully, return data
+  if (!newPhoto) {
+    res.status(422).json({
+      errors: ["Houve um erro, por favor tente novamente mais tarde."],
     });
+    return;
+  }
 
-    if (!newPhoto) {
-        res.status(422).json({
-            errors: ["There's been an error, please try again later."]
-        });
-        return;
-    }
-
-    res.status(201).json(newPhoto);
+  res.status(201).json(newPhoto);
 };
 
-// Remove a photo from db
+// Remove a photo from the DB
 const deletePhoto = async (req, res) => {
-    const { id } = req.params;
-    const reqUser = req.user;
+  const { id } = req.params;
 
-    try {
-        const photo = await Photo.findById(id);
+  const reqUser = req.user;
 
-        // Check if photo exists
-        if (!photo) {
-            res.status(404).json({ errors: ["Picture not found."] });
-            return;
-        }
+  const photo = await Photo.findById(new mongoose.Types.ObjectId(id));
 
-        // Check if photo belongs to user
-        if (!photo.userId.equals(reqUser._id)) {
-            res.status(422).json({ errors: ["You do not have permission to delete this picture."] });
-            return;
-        }
+  // Check if photo exists
+  if (!photo) {
+    res.status(404).json({ errors: ["Foto não encontrada!"] });
+    return;
+  }
 
-        await Photo.findByIdAndDelete(photo._id);
-        res.status(200).json({ id: photo._id, message: "Picture successfully deleted." });
-    } catch (error) {
-        res.status(500).json({ errors: ["An error occurred while processing your request."] });
-    }
+  // Check if photo belongs to user
+  if (!photo.userId.equals(reqUser._id)) {
+    res
+      .status(422)
+      .json({ errors: ["Ocorreu um erro, tente novamente mais tarde"] });
+    return;
+  }
+
+  await Photo.findByIdAndDelete(photo._id);
+
+  res
+    .status(200)
+    .json({ id: photo._id, message: "Foto excluída com sucesso." });
 };
 
 // Get all photos
-const getAllphotos = async(req, res) => {
-
-    const photos = await Photo.find({}).sort([["createdAt", -1]]).exec()
-
-    return res.status(200).json(photos)
-}
-
-// Get user photos
-const getUserPhotos = async(req, res) => {
-    
-    const {id} = req.params
-
-    const photos = await Photo.find({userId: id})
+const getAllPhotos = async (req, res) => {
+  const photos = await Photo.find({})
     .sort([["createdAt", -1]])
     .exec();
 
-    return res.status(200).json(photos)
-}
+  return res.status(200).json(photos);
+};
+
+// Get user photos
+const getUserPhotos = async (req, res) => {
+  const { id } = req.params;
+
+  const photos = await Photo.find({ userId: id })
+    .sort([["createdAt", -1]])
+    .exec();
+
+  return res.status(200).json(photos);
+};
+
+// Get photo by id
+const getPhotoById = async (req, res) => {
+  const { id } = req.params;
+
+  const photo = await Photo.findById(new mongoose.Types.ObjectId(id));
+
+  // Check if photo exists
+  if (!photo) {
+    res.status(404).json({ errors: ["Foto não encontrada!"] });
+    return;
+  }
+
+  res.status(200).json(photo);
+};
+
 
 module.exports = {
     insertPhoto,
     deletePhoto,
-    getAllphotos,
+    getAllPhotos,
     getUserPhotos,
+    getPhotoById
 };
