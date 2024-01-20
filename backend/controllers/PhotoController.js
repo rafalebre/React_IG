df5d97b809fe67b89afd14fe77274c8d3de817ce
Rafael Lebre <rafaellebre100@gmail.com>
2024-01-20 13:29:09 +0100

    202: Excluindo fotos corrigido

--- backend/controllers/PhotoController.js
+++ backend/controllers/PhotoController.js
@@ -1,71 +1,60 @@
-const Photo = require("../models/Photo")
-
-const mongoose = require("mongoose")
-const User = require("../models/User")
+const Photo = require("../models/Photo");
+const User = require("../models/User");
 
 // Insert a photo, with an user related to it
-const insertPhoto = async(req, res) => {
+const insertPhoto = async (req, res) => {
+    const { title } = req.body;
+    const image = req.file.filename;
+    const reqUser = req.user;
 
-    const {title} = req.body
-    const image = req.file.filename
+    const user = await User.findById(reqUser._id);
 
-    const reqUser = req.user
-
-    const user = await User.findById(reqUser._id)
- 
     // Create a photo
     const newPhoto = await Photo.create({
         image, 
         title,
         userId: user._id,
         userName: user.name,
-    })
+    });
 
-    //If photo was create succesfully, return data
-    if(!newPhoto) {
-
+    if (!newPhoto) {
         res.status(422).json({
             errors: ["There's been an error, please try again later."]
-        })
-        return
+        });
+        return;
     }
 
-
-    res.status(201).json(newPhoto)
-}
+    res.status(201).json(newPhoto);
+};
 
 // Remove a photo from db
-const deletePhoto = async(req, res) => {
-
-    const {id} = req.params
-
-    const reqUser = req.user
+const deletePhoto = async (req, res) => {
+    const { id } = req.params;
+    const reqUser = req.user;
 
     try {
-        const photo =  Photo.findById(mongoose.Types.ObjectId(id))
+        const photo = await Photo.findById(id);
 
-    // Check if photo exists
-    if(!photo) {
-        res.status(404).json({errors:["Picture not found."]})
-        return
-    }
+        // Check if photo exists
+        if (!photo) {
+            res.status(404).json({ errors: ["Picture not found."] });
+            return;
+        }
 
-    // Check if photo belongs to user
-    if(!photo.userId.equals(reqUser._id)) {
-        res.status(422).json({errors: ["There's been an error, please try again later"]})
-    }
+        // Check if photo belongs to user
+        if (!photo.userId.equals(reqUser._id)) {
+            res.status(422).json({ errors: ["You do not have permission to delete this picture."] });
+            return;
+        }
 
-    await Photo.findByIdAndDelete(photo._id)
-
-    res.status(200).json({id: photo._id, message: "Picture successfully deleted."})
+        await Photo.findByIdAndDelete(photo._id);
+        res.status(200).json({ id: photo._id, message: "Picture successfully deleted." });
     } catch (error) {
-        res.status(404).json({errors:["Picture not found."]})
-        return
+        res.status(500).json({ errors: ["An error occurred while processing your request."] });
     }
-
-}
+};
 
 module.exports = {
     insertPhoto,
     deletePhoto
-}
+};
