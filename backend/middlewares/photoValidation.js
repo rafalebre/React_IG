c711a60f0aef825b8974690c59fd8f93afbd5193
Rafael Lebre <rafaellebre100@gmail.com>
2024-01-21 14:40:32 +0100

    206: Atualização de foto

--- backend/middlewares/photoValidation.js
+++ backend/middlewares/photoValidation.js
@@ -1,17 +1,17 @@
-const {body} = require("express-validator")
+const { body } = require("express-validator")
 
 const photoInsertValidation = () => {
     return [
         body("title")
-        .not()
-        .equals("undefined")
-        .withMessage("The title is mandatory.")
-        .isString()
-        .withMessage("The title is mandatory.")
-        .isLength({min: 3})
-        .withMessage("The title must have at least 3 characters."),
-        body("image").custom((value, {req}) => {
-            if(!req.file) {
+            .not()
+            .equals("undefined")
+            .withMessage("The title is mandatory.")
+            .isString()
+            .withMessage("The title is mandatory.")
+            .isLength({ min: 3 })
+            .withMessage("The title must have at least 3 characters."),
+        body("image").custom((value, { req }) => {
+            if (!req.file) {
                 throw new Error("The image is mandatory.")
             }
             return true;
@@ -19,6 +19,18 @@ const photoInsertValidation = () => {
     ]
 }
 
+const photoUpdateValidation = () => {
+    return [
+        body("title")
+            .optional()
+            .isString()
+            .withMessage("The title is mandatory")
+            .isLength({ min: 3 })
+            .withMessage("The title must have at least 3 characters."),
+    ]
+}
+
 module.exports = {
     photoInsertValidation,
+    photoUpdateValidation,
 }
