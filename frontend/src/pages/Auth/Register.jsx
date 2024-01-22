1674c74c02c18454749f3e4292f09375b8e68b13
Rafael Lebre <rafaellebre100@gmail.com>
2024-01-22 21:40:15 +0100

    217: CSS do form de autenticação

--- frontend/src/pages/Auth/Register.jsx
+++ frontend/src/pages/Auth/Register.jsx
@@ -13,7 +13,7 @@ const Register = () => {
   }
 
   return (
-    <div>
+    <div id="register">
       <h2>ReactGram</h2>
       <p className="subtitle">Register to start interacting with other people.</p>
       <form onSubmit={handleSubmit}>
