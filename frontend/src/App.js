7999eebc96814a7d7d9b37e63a20a27055abbe42
Rafael Lebre <rafaellebre100@gmail.com>
2024-01-22 17:43:03 +0100

    213: Criando componente de navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,20 +3,27 @@ import './App.css';
 // Router
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 
+// Components
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
 // Pages
 import Home from './pages/Home/Home';
 import Login from './pages/Auth/Login';
 import Register from './pages/Auth/Register';
 
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
