a6dc13a078319015e968e94daf4f6883abf739cf
Rafael Lebre <rafaellebre100@gmail.com>
2024-01-23 21:16:41 +0100

    233: Slice de login

--- frontend/src/slices/authSlice.jsx
+++ frontend/src/slices/authSlice.jsx
@@ -30,6 +30,21 @@ export const logout = createAsyncThunk("auth/logout", async () => {
     await authService.logout()
 })
 
+// Login an user
+export const login = createAsyncThunk("auth/login",
+    async (user, thunkAPI) => {
+
+        const data = await authService.login(user)
+
+        // check for errors
+        if (data.errors) {
+            return thunkAPI.rejectWithValue(data.errors[0])
+        }
+
+        return data
+
+    })
+
 export const authSlice = createSlice({
     name: "auth",
     initialState,
@@ -41,10 +56,11 @@ export const authSlice = createSlice({
         }
     },
     extraReducers: (builder) => {
-        builder.addCase(register.pending, (state) => {
-            state.loading = true
-            state.error = false
-        })
+        builder
+            .addCase(register.pending, (state) => {
+                state.loading = true
+                state.error = false
+            })
             .addCase(register.fulfilled, (state, action) => {
                 state.loading = false
                 state.success = true
@@ -60,6 +76,21 @@ export const authSlice = createSlice({
                 state.loading = false
                 state.success = false
                 state.error = null
+                state.user = null
+            })
+            .addCase(login.pending, (state) => {
+                state.loading = true
+                state.error = false
+            })
+            .addCase(login.fulfilled, (state, action) => {
+                state.loading = false
+                state.success = true
+                state.error = null
+                state.user = action.payload
+            })
+            .addCase(login.rejected, (state, action) => {
+                state.loading = false
+                state.error = action.payload
                 state.user = null
             })
     }
