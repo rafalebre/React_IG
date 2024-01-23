16b2271ff060c17dd07892c531f15f422b614227
Rafael Lebre <rafaellebre100@gmail.com>
2024-01-23 15:09:36 +0100

    218: Criando states do form de cadastro

--- frontend/src/pages/Auth/Register.jsx
+++ frontend/src/pages/Auth/Register.jsx
@@ -1,15 +1,28 @@
 import "./Auth.css";
 
 // Components
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 
 // Hooks
 import { useState, useEffect } from "react";
 
 const Register = () => {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [confirmPassword, setConfirmPassword] = useState("")
 
   const handleSubmit = (e) => {
     e.preventDefault()
+
+    const user = {
+      name,
+      email,
+      password,
+      confirmPassword
+    }
+
+    console.log(user);
   }
 
   return (
@@ -17,11 +30,29 @@ const Register = () => {
       <h2>ReactGram</h2>
       <p className="subtitle">Register to start interacting with other people.</p>
       <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Name" />
-        <input type="email" placeholder="E-mail" />
-        <input type="password" placeholder="Password" />
-        <input type="password" placeholder="Confirm your password" />
-        <input type="submit" value="Register" />
+        <input
+          type="text"
+          placeholder="Name"
+          onChange={(e) => setName(e.target.value)}
+          value={name || ""} />
+        <input
+          type="email"
+          placeholder="E-mail"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email || ""} />
+        <input
+          type="password"
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password || ""} />
+        <input
+          type="password"
+          placeholder="Confirm your password"
+          onChange={(e) => setConfirmPassword(e.target.value)}
+          value={confirmPassword || ""} />
+        <input
+          type="submit"
+          value="Register" />
       </form>
       <p>
         Already registered? <Link to="/login">Click here</Link>
