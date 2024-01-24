a5bc5a4378af8c8295b17fb531b62e63ca643839
Rafael Lebre <rafaellebre100@gmail.com>
2024-01-24 20:08:24 +0100

    245: Criando página de postagem de fotos

---
+++ frontend/src/pages/Profile/Profile.jsx
@@ -0,0 +1,23 @@
+import "./Profile.css"
+
+import {uploads} from "../../utils/config"
+
+// components
+import Message from "../../components/Message"
+import { Link } from "react-router-dom"
+import { BsFillEyeFill, BsPencilFill, BsXLg } from "react-icons/bs"
+
+// Hooks
+import { useState, useEffect, useRef } from "react"
+import { UseSelector, useDispatch } from "react-redux"
+import { useParams } from "react-router-dom"
+
+// Redux
+
+const Profile = () => {
+  return (
+    <div>Profile</div>
+  )
+}
+
+export default Profile
