8208e19515eaf1ca3c04b681b967522c25b6f1f9
Rafael Lebre <rafaellebre100@gmail.com>
2024-01-24 15:19:01 +0100

    235: Estruturando página de edição de perfil

---
+++ frontend/src/pages/EditProfile/EditProfile.jsx
@@ -0,0 +1,34 @@
+import "./EditProfile.css";
+
+const EditProfile = () => {
+  
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
+    <div id="edit-profile">
+      <h2>Edit your data</h2>
+      <p className="subtitle">Add a profile imange and tell a bit about yourself...</p>
+      {/* preview da imagem */}
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Name" />
+        <input type="email" placeholder="E-mail" disabled/>
+        <label>
+          <span>Profile Image:</span>
+          <input type="file" />
+        </label>
+        <label>
+          <span>Bio:</span>
+          <input type="text" placeholder="Profile description" />
+        </label>
+        <label>
+          <span>Wanna change your password?</span>
+          <input type="password" placeholder="Type your new password" />
+        </label>
+        <input type="submit" value="Update" />
+      </form>
+    </div>
+  
+}
+
+export default EditProfile
