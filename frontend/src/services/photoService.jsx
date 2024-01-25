2d102f0eb17e7da48ea4363e62678c542b47bd5a
Rafael Lebre <rafaellebre100@gmail.com>
2024-01-25 19:52:42 +0100

    259: Service de exclusão de fotos

--- frontend/src/services/photoService.jsx
+++ frontend/src/services/photoService.jsx
@@ -34,13 +34,29 @@ const getUserPhotos = async (id, token) => {
     } catch (error) {
         console.log(error)
     }
+}
 
+// Delete a photo
+const deletePhoto = async(id, token) => {
+
+    const config = requestConfig("DELETE", null, token)
+
+    try {
+        
+        const res = await fetch(api + "/photos/" + id, config)
+            .then((res) => res.json())
+            .catch((err) => err)
+
+    } catch (error) {
+        console.log(error)
+    }
 
 }
 
 const photoService = {
     publishPhoto,
-    getUserPhotos
+    getUserPhotos,
+    deletePhoto
 }
 
 export default photoService
