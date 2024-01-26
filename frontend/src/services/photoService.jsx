7e96078dbc67518bfa61cbd6ec77cdb0559bde97
Rafael Lebre <rafaellebre100@gmail.com>
2024-01-26 18:38:24 +0100

    262: Service de edição de fotos

--- frontend/src/services/photoService.jsx
+++ frontend/src/services/photoService.jsx
@@ -52,13 +52,31 @@ const deletePhoto = async(id, token) => {
     } catch (error) {
         console.log(error)
     }
+}
 
+// Update a photo
+const updatePhoto = async(data, id, token) => {
+
+    const config = ("PUT", data, token)
+
+    try {
+        
+        const res = await fetch(api + "/photos/" + id, config)
+                .then((res) => res.json())
+                .catch((err) => err)
+
+                return res
+
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 const photoService = {
     publishPhoto,
     getUserPhotos,
     deletePhoto,
+    updatePhoto,
 }
 
 export default photoService
