8efa717ced241a51e20b6c9f1310ee9a9d1efd9c
Rafael Lebre <rafaellebre100@gmail.com>
2024-01-27 11:30:27 +0100

    272: Funcionalidade de like da foto

--- frontend/src/pages/Photo/Photo.jsx
+++ frontend/src/pages/Photo/Photo.jsx
@@ -13,7 +13,8 @@ import { useSelector, useDispatch } from "react-redux"
 import { useParams } from "react-router-dom"
 
 // Redux
-import { getPhoto } from "../../slices/photoSlice"
+import { getPhoto, like } from "../../slices/photoSlice"
+import LikeContainer from "../../components/LikeContainer"
 
 
 const Photo = () => {
@@ -31,7 +32,10 @@ const Photo = () => {
         dispatch(getPhoto(id))
     }, [dispatch, id])
 
-    // Like and comment
+    const handleLike = () => {
+      dispatch(like(photo._id))
+
+    }
 
     if(loading) {
         return <p>Loading...</p>
@@ -41,6 +45,7 @@ const Photo = () => {
   return (
     <div id="photo">
         <PhotoItem photo={photo} />
+        <LikeContainer photo={photo} user={user} handleLike={handleLike} />
     </div>
   )
 }
