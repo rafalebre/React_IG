0eefdec2e7c2ec25314f62df35d34c2f5dc8e149
Rafael Lebre <rafaellebre100@gmail.com>
2024-01-27 14:33:47 +0100

    282: Implementando formulário de busca

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -17,6 +17,8 @@ const Navbar = () => {
     const { auth } = useAuth()
     const { user } = useSelector((state) => state.auth)
 
+    const [query, setQuery] = useState("")
+
     const navigate = useNavigate()
 
     const dispatch = useDispatch()
@@ -28,12 +30,20 @@ const Navbar = () => {
         navigate("/login")
     }
 
+    const handleSearch = (e) => {
+        e.preventDefault()
+
+        if(query) {
+            return navigate(`/search?q=${query}`)
+        }
+    }
+
     return (
         <nav id="nav">
             <Link to="/">ReactGram</Link>
-            <form id="search-form">
+            <form id="search-form" onSubmit={handleSearch}  >
                 <BsSearch />
-                <input type="text" placeholder="Search" />
+                <input type="text" placeholder="Search" onChange={(e) => setQuery(e.target.value)} />
             </form>
             <ul id="nav-links">
                 {auth ?
