157364ee5bd8b57a672cb42edefa6b72b2223925
Rafael Lebre <rafaellebre100@gmail.com>
2024-01-27 13:36:41 +0100

    280: Finalizando home

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -17,7 +17,7 @@ const Home = () => {
 
   const dispatch = useDispatch()
 
-  const resetMessage = useResetComponentMessage()
+  const resetMessage = useResetComponentMessage(dispatch)
 
   const {user} = useSelector((state) => state.auth)
   const {photos, loading} = useSelector((state) => state.photo)
@@ -39,7 +39,20 @@ const Home = () => {
   }
 
   return (
-    <div>Home</div>
+    <div id="home">
+      {photos && photos.map((photo) => (
+      <div key={photo._id}>
+        <PhotoItem photo={photo} />
+        <LikeContainer photo={photo} user={user} handleLike={handleLike} />
+        <Link className="btn" to={`/photos/${photo._id}`} >View more</Link>
+      </div>)
+      )}
+      {photos && photos.length === 0 && (
+        <h2 className="no-photos">
+          There are no pictures published yet, <Link to={`/users/${user._id}`} >Click here</Link>
+        </h2>
+      )}
+    </div>
   )
 }
 
