d3ad8f7064c3473f87d3dcc8ff867eb43af56273
Rafael Lebre <rafaellebre100@gmail.com>
2024-01-27 14:25:18 +0100

    281: Criando página de busca

---
+++ frontend/src/pages/Search/Search.jsx
@@ -0,0 +1,22 @@
+import "./Search.css"
+
+// hooks
+import { useEffect } from "react"
+import {useSelector, useDispatch} from "react-redux"
+import { useResetComponentMessage } from "../../hooks/useResetComponentMessage"
+
+// components
+import LikeContainer from "../../components/LikeContainer"
+import PhotoItem from "../../components/PhotoItem"
+import { Link } from "react-router-dom"
+
+// Redux
+
+
+const Search = () => {
+  return (
+    <div>Search</div>
+  )
+}
+
+export default Search
