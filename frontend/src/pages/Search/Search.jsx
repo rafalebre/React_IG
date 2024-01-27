3069dd0b74c93e98e249346316dc1d778a886780
Rafael Lebre <rafaellebre100@gmail.com>
2024-01-27 18:42:09 +0100

    285: Concluindo busca de fotos

--- frontend/src/pages/Search/Search.jsx
+++ frontend/src/pages/Search/Search.jsx
@@ -12,11 +12,53 @@ import PhotoItem from "../../components/PhotoItem"
 import { Link } from "react-router-dom"
 
 // Redux
-
+import { searchPhotos, like } from "../../slices/photoSlice"
 
 const Search = () => {
+  const query = useQuery()
+  const search = query.get("q")
+
+  const dispatch = useDispatch()
+
+  const resetMessage = useResetComponentMessage(dispatch)
+
+  const {user} = useSelector(state => state.auth)
+  const {photos, loading} = useSelector(state => state.photo)
+
+  // Load photos
+  useEffect(() => {
+
+    dispatch(searchPhotos(search))
+
+  }, [dispatch, search])
+
+    // Like a photo
+    const handleLike = (photo) => {
+      dispatch(like(photo._id))
+  
+      resetMessage()
+    }
+  
+    if(loading) {
+      return <p>Loading...</p>
+    }
+
   return (
-    <div>Search</div>
+    <div id="search">
+      <h2>You're looking for: {search}</h2>
+      {photos && photos.map((photo) => (
+        <div key={photo._id}>
+        <PhotoItem photo={photo} />
+        <LikeContainer photo={photo} user={user} handleLike={handleLike} />
+        <Link className="btn" to={`/photos/${photo._id}`} >View more</Link>
+      </div>
+      ))}
+      {photos && photos.length === 0 && (
+        <h2 className="no-photos">
+          No results were found for your search...
+        </h2>
+      )}
+    </div>
   )
 }
 
