736fb519e64135a747e42001f9966a2ffa9f16d0
Rafael Lebre <rafaellebre100@gmail.com>
2025-01-18 11:37:03 +0100

    backend changes to deploy at Render

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,10 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 // solve CORS
-app.use(cors({ credentials: true, origin: "http://localhost:3000"}))
+app.use(cors({ 
+    credentials: true, 
+    origin: process.env.FRONTEND_URL || "http://localhost:3000"
+  }))
 
 // Upload directory
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")))
