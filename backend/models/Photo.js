a9ce7d4d5e59471196c4ca4c7b827b444bfdaff8
Rafael Lebre <rafaellebre100@gmail.com>
2026-01-05 15:21:00 +0100

    fix: add comment schema to generate _id for delete functionality

--- backend/models/Photo.js
+++ backend/models/Photo.js
@@ -1,11 +1,20 @@
 const mongoose = require("mongoose")
 const {Schema} = mongoose
 
+const commentSchema = new Schema({
+    comment: String,
+    userName: String,
+    userImage: String,
+    userId: mongoose.ObjectId
+}, {
+    timestamps: true
+})
+
 const photoSchema = new Schema({
     image: String,
     title: String,
     likes: Array,
-    comments: Array,
+    comments: [commentSchema],
     userId: mongoose.ObjectId,
     userName: String,
 }, {
